f94420e4c2de0e93dc8ac70daa28e4b91e1e889e
malhotra5 <rmalhotra651@student.fuhsd.org>
2019-01-05 22:34:28 -0800

    results and code addition

---
+++ code/basics.py
@@ -0,0 +1,14 @@
+from big_ol_pile_of_manim_imports import *
+
+class Shapes(Scene):
+    def construct(self):
+        #Making shapes
+        circle = Circle()
+        square = Square()
+        triangle=Polygon(np.array([0,0,0]),np.array([1,1,0]),np.array([1,-1,0]))
+
+        #Showing shapes
+        self.play(ShowCreation(circle))
+        self.play(FadeOut(circle))
+        self.play(GrowFromCenter(square))
+        self.play(Transform(square,triangle))
